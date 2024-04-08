4d58b554acce3673668807057a06ba59c4502dd6
TheruniKavindya <theruniks1998@gmail.com>
2024-04-08 11:40:06 +0530

    fetcher

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,7 +5,7 @@ import useSWR from "swr";
 import fetcher from "../../utils/fetcher";
 
 export default function Landing() {
-  const [data, setData] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -22,20 +22,18 @@ export default function Landing() {
         .then((res) => res.json())
         .then((res) => {
           if (res) {
-            console.log("res", res);
-            setData(res.results);
+            setPopularMovies(res.results);
           }
-          console.log(res, "not working");
         });
     })();
   }, []);
 
-  // const { data } = useSWR(
-  //   "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-  //   fetcher
-  // );
+  const { data, error, isLoading } = useSWR(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    fetcher
+  );
 
-  // console.log(data);
+  // console.log(data, error, isLoading);
 
   return (
     <div className="flex flex-col w-full h-full py-28 px-10 bg-movieClub-bg-primary">
@@ -43,7 +41,23 @@ export default function Landing() {
         Most Rated
       </p>
       <div className="flex flex-col md:flex-row justify-between gap-y-10 flex-wrap">
-        {data.results.map((movie) => (
+        <p className="text-white">{data}</p>
+        {data.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            title={movie.title}
+            rating={4}
+            year={new Date(movie.release_date).getFullYear()}
+            imageUrl={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          />
+        ))}
+      </div>
+
+      <p className="text-xl font-montserrat-regular text-movieClub-white my-3">
+        Most Popular
+      </p>
+      <div className="flex flex-col md:flex-row justify-between gap-y-10 flex-wrap">
+        {popularMovies.map((movie) => (
           <MovieCard
             key={movie.id}
             title={movie.title}
--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -2,12 +2,12 @@ const fetcher = (url) => {
   fetch(url, {
     method: "GET",
     headers: {
-      "Content-Type": "application/json",
+      accept: "application/json",
       Authorization: `Bearer ${import.meta.env.VITE_APP_ACCESS_TOKEN}`,
     },
   })
     .then((res) => res.json())
-    .then((res) => res);
+    .then((res) => res.results);
 };
 
 export default fetcher;
